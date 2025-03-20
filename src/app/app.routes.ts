06c8ab1ad3c2968351ac3a8a4cd055b5f79dab77
watter08 <watter08@hotmail.es>
2025-03-19 22:58:04 -0400

    Routes and pages login, course and students hhave been created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StudentsComponent } from './pages/students/students.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'students', component: StudentsComponent }, 
+  { path: '**', redirectTo: '', pathMatch: 'full' } 
+];
---
+++ src/app/pages/students/students.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students',
+  imports: [],
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.scss'
+})
+export class StudentsComponent {
+
+}
